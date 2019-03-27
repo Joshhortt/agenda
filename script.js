5dd677dd15cdd42980995da657f89a76f9bcf4d0
Jose Horta <joshhortt@yahoo.com>
2019-03-27 16:12:11 +0000

    add new function toggleAll.

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-var tasksList = {
+var taskList = {
   tasks: [],
   displayTasks: function()  {
-    console.log("My Tasks", this.tasks);
+    if (this.tasks.length === 0)  {
+       console.log("your task list is empty!");
+     } else {
+      console.log("My Tasks:");
+      for (var i = 0 ; i < this.tasks.length; i++) {
+        if (this.tasks[i].completed === true) {
+         console.log("(x)", this.tasks[i].taskText);
+        } else  {
+         console.log("( )", this.tasks[i].taskText);
+       }
+      }
+     }
   },
 
   addTask: function(taskText)  {
-    this.task.push({
+    this.tasks.push({
     taskText: taskText,
     completed: false
   });
@@ -13,17 +24,45 @@ var tasksList = {
   },
 
   changeTask: function(position, taskText)  {
-   this.tasks[position].taskText = taskText;
-   this.displayTasks();
-   },
+  this.tasks[position].taskText = taskText;
+  this.displayTasks();
+  },
 
-deleteTask: function(position)  {
+ deleteTask: function(position)  {
   this.tasks.splice(position, 1);
   this.displayTasks();
-},
-toggleCompleted: function(position)  {
-  var task = this.tasks[position];
-  task.completed = !task.completed;
-  this.displayTasks();
-}
+  },
+
+  toggleCompleted: function(position)  {
+    var task = this.tasks[position];
+    task.completed = !task.completed;
+    this.displayTasks();
+  },
+
+  toggleAll: function()  {
+    var totalTasks = this.tasks.length;
+    var completedTasks = 0;
+
+// Get the number of completed tasks.
+     for (var i = 0 ; i < totalTasks; i++) {
+        if (this.tasks[i].completed === true) {
+        }
+     }
+
+// Case 1: if everything ´s true, make everything false.
+  if (completedTasks === totalTasks) {
+
+// Make everything "for" false
+  for (var i = 0; i < totalTasks; i++) {
+        this.tasks[i].completed = false;
+    }
+// Case 2: Otherwise "else" make everything true.
+   } else  {
+     for (var i = 0; i < totalTasks; i++) {
+        this.tasks[i].completed = true;
+     }
+   }
+
+   this.displayTasks();
+  }
 };
